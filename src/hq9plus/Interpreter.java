fd61ebc42535c6d4450bb7d380f351c0554b5f63
alfphaderp <36178918+alfphaderp@users.noreply.github.com>
2018-04-15 23:07:49 -0700

    Fixed a bug with printing too many or too few newlines

--- src/hq9plus/Interpreter.java
+++ src/hq9plus/Interpreter.java
@@ -18,19 +18,19 @@ public class Interpreter {
 				output += "Hello, world!\n";
 				break;
 			case 'Q':
-				output += str;
+				output += str + "\n";
 				break;
 			case '9':
 				for(int j = 99; j > 1; j--)
-					output += j + " bottles of beer on the wall, " + j + " bottles of beer.\nTake one down pass it around, " + (j - 1) + " bottles of beer on the wall.\n";
-				output += "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down pass it around, no more bottles of beer on the wall.\n";
-				output += "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 more bottles of beer on the wall.";
+					output += j + " bottles of beer on the wall, " + j + " bottles of beer.\nTake one down pass it around, " + (j - 1) + " bottles of beer on the wall.\n\n";
+				output += "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down pass it around, no more bottles of beer on the wall.\n\n";
+				output += "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 more bottles of beer on the wall.\n";
 				break;
 			case '+':
 				accumulator++;
 			}
 		}
 		
-		return output;
+		return output.substring(0, output.length() - 1);
 	}
 }
